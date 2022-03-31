f3dea004e3cc9defa8df25956ce9965432f961a6
Grishma Bhattarai <itsgrishmab@gmail.com>
2022-03-31 15:45:43 -0500

    Some Codes for Practice

---
+++ temp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+Author: Grishma Bhattarai
+"""
+
+print('This is a practice run for Git Push and Pull')
+
+print ('My name is Grishma :)')
+
+x = 2 + 4
+print(x)
+
+    
+    
